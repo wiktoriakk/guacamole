6d1c911030f9c476e50f2acbc26e491aa3e04aad
wiktoriakk <161590875+wiktoriakk@users.noreply.github.com>
2024-02-28 14:14:02 +0100

    Add files via upload

---
+++ git2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    cout << "AWOKADO" << endl;
+    // Rysowanie awokado
+    cout << "    _____" << endl;
+    cout << "   /     \\" << endl;
+    cout << "  |       |" << endl;
+    cout << "  |  O O  |" << endl;
+    cout << "  |   ∆   |" << endl;
+    cout << "   \\_____/ " << endl;
+
+    return 0;
+}
